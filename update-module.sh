cc4404af20b94ae16ab26f89d2bf16cdec036256
Edward George <edwardgeorge@gmail.com>
2023-11-09 00:06:07 +0000

    fix update scripts for committing

--- update-module.sh
+++ update-module.sh
@@ -2,4 +2,3 @@
 set -ex
 git bundle create ${toplevel}/${sha1}.bundle HEAD
 git -C $toplevel -c protocol.file.allow=always fetch ${toplevel}/${sha1}.bundle HEAD:archive/$name
-git diff-index --quiet HEAD || git commit -m "update submodules"
