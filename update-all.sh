45842e67b15b9494e4ad35d595c6d2fb0e6a26a4
Edward George <edwardgeorge@gmail.com>
2023-11-09 00:27:03 +0000

    add missing commit to update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -2,6 +2,5 @@
 set -ex
 git submodule update --init --remote
 git add -u
-#git submodule foreach 'git bundle create $toplevel/$sha1.bundle HEAD && git -C $toplevel -c protocol.file.allow=always fetch $toplevel/$sha1.bundle HEAD:archive/$name'
 git submodule foreach '$toplevel/update-module.sh'
-git diff-index --quiet HEAD
+git diff-index --quiet HEAD || git commit -m "update submodules"
