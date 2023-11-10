6995d61fafd43f685911e1f4a4dea89d7c902e36
Edward George <edwardgeorge@gmail.com>
2023-11-10 18:53:00 +0000

    update scripts

--- update-all.sh
+++ update-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-git submodule update --init --remote
+git submodule update --init --remote --filter=tree:0
 git add -u
 mkdir -p bundles
 git submodule foreach '$toplevel/update-module.sh'
--- update-module.sh
+++ update-module.sh
@@ -1,4 +1,25 @@
 #!/usr/bin/env bash
 set -ex
-git bundle create ${toplevel}/bundles/${sha1}.bundle HEAD
-git -C $toplevel -c protocol.file.allow=always fetch ${toplevel}/bundles/${sha1}.bundle HEAD:archive/$name
+
+current_sha="$(git rev-parse --verify HEAD)"
+branch_name="archive/$name"
+bundle_name="${toplevel}/bundles/${current_sha}.bundle"
+if res="$(git -C $toplevel ls-remote --heads --exit-code origin $branch_name)"
+then
+    remote_sha=$(echo $res | awk '{print $1}')
+    # create a partial bundle and fetch necessary local commits
+    if [ "$remote_sha" != "$current_sha" ]
+    then
+        echo "changes in branch, creating bundle with differences..."
+        git bundle create "${bundle_name}" "${remote_sha}..HEAD"
+        git -C "${toplevel}" fetch --depth=2 --filter=tree:0 origin "${remote_sha}"
+    fi
+else
+    echo "branch ${branch_name} does not exist. creating bundle from scratch..."
+    git bundle create "${bundle_name}" HEAD
+fi
+if [ -f "${bundle_name}" ]
+then
+    git bundle verify "${bundle_name}"
+    git -C "${toplevel}" -c protocol.file.allow=always fetch "${bundle_name}" "HEAD:${branch_name}"
+fi
