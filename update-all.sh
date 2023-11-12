315774d9cf73ad12cf085665081cb677a441abe9
Edward George <edwardgeorge@gmail.com>
2023-11-12 15:04:53 +0000

    Update update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -4,6 +4,7 @@ mkdir -p bundles
 for sub in $(git submodule status | awk '{print $2}')
 do
     git submodule update --init --remote --filter=tree:0 "$sub"
+    git add "$sub"
     git submodule foreach '$toplevel/update-module.sh;'
 done
 git diff-index --quiet HEAD || git commit -m "update submodules\n\n[skip ci]"
