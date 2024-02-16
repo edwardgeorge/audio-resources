65ca1238bca2c257ba38d08dff32205df8c68bdb
Edward George <edwardgeorge@gmail.com>
2024-02-16 10:14:35 +0000

    use prob-check-repo for update process

--- update-all.sh
+++ update-all.sh
@@ -3,8 +3,12 @@ set -ex
 mkdir -p bundles
 for sub in $(git submodule status | awk '{print $2}')
 do
-    git submodule update --init --remote --filter=tree:0 "$sub"
-    git add "$sub"
-    git submodule foreach '$toplevel/update-module.sh;'
+    if prob-check-repo -d .repodata "$sub" check --seed "$(git show-ref -s HEAD)" 
+    then
+        git submodule update --init --remote --filter=tree:0 "$sub"
+        git add "$sub"
+        git submodule foreach '$toplevel/update-module.sh;'
+    fi
 done
+git add .repodata
 git diff-index --quiet HEAD || git commit -m "update submodules" -m "[skip ci]"
