b5b45b163c34029159bbfe2acae09640f5a65bec
Edward George <edwardgeorge@gmail.com>
2024-02-19 19:02:51 +0000

    update scripts for running in subdir

--- update-all.sh
+++ update-all.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 set -ex
 mkdir -p bundles
+cd "${META_DIR:-.meta}"
+export META_DIR="$(pwd)"
+cd -
+test -f "${META_DIR}/.repodata"
 if [ -z ${SEED+x} ]
 then
     export SEED="$(git log -1 --format='format:%H')";
 fi
 for sub in $(git submodule status | awk '{print $2}')
 do
-    if prob-check-repo -d .repodata -n "$sub" check --seed "${SEED:+${SEED}+${sub}}" 
+    if prob-check-repo -d "${META_DIR}/.repodata" -n "$sub" check --seed "${SEED:+${SEED}+${sub}}" 
     then
         git submodule update --init --remote --filter=tree:0 "$sub"
         git add "$sub"
-        git submodule foreach '$toplevel/update-module.sh;'
+        git submodule foreach "${META_DIR}/update-module.sh;"
     fi
 done
-git add .repodata
 git diff-index --quiet HEAD || git commit -m "update submodules" -m "[skip ci]"
+git add .repodata
+git -C "${META_DIR}" diff-index --quiet HEAD || git -C "${META_DIR}" commit -m "update .repometa" -m "[skip ci]"
